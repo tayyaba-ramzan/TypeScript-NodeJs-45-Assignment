423f0362ab912d8c4325db0bb2b26d1192a65a68
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-03-18 19:24:29 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -38,7 +38,7 @@ console.log("Stripped:",PersonName.trim());
 // Assignment NO 7
  console.log(5+3);   // Addition
  console.log(10-2);  // Subtraction
- console.log(4*2);   // Multipication
+ console.log(4*2);   // Multiplication
  console.log(64/8);  // Division  
             //  ------------xxx------------ 
 
